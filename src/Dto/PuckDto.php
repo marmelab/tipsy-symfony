73d53fe336d3e3a9d59a7fd39d5a3634f318cf1b
Guillaume <guillaume.billey@gmail.com>
2021-02-10 15:44:17 +0100

    fix(color): add missing color for pucks

---
+++ src/Dto/PuckDto.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Game;
+
+class  PuckDto{
+
+    public $color;
+    public $position;
+    public $flipped = false;
+
+    public function __construct($color, $position, $flipped) {
+        $this->color = $color;
+        $this->position = $position;
+        $this->flipped = $flipped;
+
+    }
+    public static function getPucksDto(Game $game){
+        $pucks = array();
+        foreach ($game->graph->vertices as $position => $vertex) {
+            if ($vertex->getValue() && array_key_exists(Game::COLOR_KEY, $vertex->getValue())){
+                array_push($pucks, 
+                new PuckDto(
+                    $vertex->getValue()[Game::COLOR_KEY], 
+                    ["x"=>strval($position)[0],"y"=>strval($position)[1]],
+                    $vertex->getValue()[Game::FLIPPED_KEY]));
+            }
+        };
+        return array_values($pucks);
+    }
+}
