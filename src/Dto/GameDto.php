a88e97e1e7d5fd71da0099019602b6084b702076
Guillaume <guillaume.billey@gmail.com>
2021-02-10 21:02:45 +0100

    feat(replace): replace api

--- src/Dto/GameDto.php
+++ src/Dto/GameDto.php
@@ -8,6 +8,7 @@ class  GameDto{
 
     public $id;
     public $players;
+    public $scores;
 
     public $pucks = [];
 
@@ -16,6 +17,7 @@ class  GameDto{
     public function __construct(Game $game){
         $this->id = $game->id;
         $this->players = PlayerDto::getPlayerDtos($game);
+        $this->scores = $game->scores;
         $this->fallenPucks = array_values($game->fallenPucks);
         $this->pucks = PuckDto::getPucksDto($game);
     }
